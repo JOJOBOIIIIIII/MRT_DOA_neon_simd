1f2ef1af7a1a95cb8cf4bf2982b75469d7e5b2dd
Joel Boyer <joel.boyer@mail.mcgill.ca>
2023-10-25 14:55:05 -0400

    Program with real and imaginary separated is done. need to set values in matrix and test results

--- sample_cov_matrix.c
+++ sample_cov_matrix.c
@@ -1,52 +1,71 @@
 #include <stdio.h>
-#include <gsl/gsl_complex.h>
-#include <gsl/gsl_complex_math.h>
 #include <gsl/gsl_matrix.h>
 #include <gsl/gsl_blas.h>
 
-#define I(x,y) (gsl_complex_rect(x,y))
+//Could be done simpler/shorter with blas, because it can add operators and constant to matrices when multiplying, in one line
+
 
 int main(){
-	printf("hello world\n");
+	printf("hello ha world\n");
 
 	int n = 5;
-	gsl_complex samples[]={ I(0,0), I(0,0) ,I(0,0), I(0,0) ,I(0,0), 
-				I(0,0), I(0,0), I(0,0), I(0,0), I(0,0),
-				I(0,0), I(0,0), I(0,0), I(0,0) ,I(0,0),
-				I(0,0), I(0,0), I(0,0), I(0,0), I(0,0),
-				I(0,0), I(0,0), I(0,0), I(0,0), I(0,0)};
+
+	double samples_re[] = { 0,0,0,0,0,
+				0,0,0,0,0,
+				0,0,0,0,0,
+				0,0,0,0,0,
+				0,0,0,0,0
+				}
+
+	double samples_im[] = { 0,0,0,0,0,
+				0,0,0,0,0,
+				0,0,0,0,0,
+				0,0,0,0,0,
+				0,0,0,0,0
+				}
 
 
-	gsl_matrix_complex* matrix = gsl_matrix_complex_alloc(5,5);
-		
+	gsl_matrix* matrix_sample_re = gsl_matrix_alloc(5,5);
+	gsl_matrix* matrix_sample_im = gsl_matrix_alloc(5,5);
+
+
 	for (int i = 0; i < 5; i++) {
      	   for (int j = 0; j < 5; j++) {
-        	    gsl_matrix_complex_set(matrix, i, j, samples[i * 5 + j]);
-        	}
+        	
+	   }
     	}
-	
-	gsl_matrix_complex* matrix_result = gsl_matrix_complex_alloc(5,5);
 
-	//This takes the conjugate transpose according to ChatGPT
-	gsl_matrix_complex *identity = gsl_matrix_complex_alloc(5, 5);
-    	gsl_matrix_complex_set_identity(identity);
-	gsl_blas_zgerc(gsl_complex_rect(1.0, 0.0), matrix, matrix, identity);
-	
-	gsl_matrix_complex *matrix_hermit = identity;
+	gsl_matrix* matrix_hermit_re = gsl_matrix_alloc(5,5); 
+	gsl_matrix_transpose_memcpy(matrix_hermit_re, matrix_sample_re); // Transpose real part
 
-	// Complex Double (z), General (ge), Matrix Multiplication (mm)
-	gsl_blas_zgemm(CblasNoTrans, CblasNoTrans, gsl_complex_rect(1.0, 0.0), matrix, matrix_hermit, gsl_complex_rect(0.0, 0.0), matrix_result);
-	
-//	for (int i = 0; i < n; i++)  /* OUT OF RANGE ERROR */
-  //  		for (int j = 0; j < n; j++)
-    //  			printf ("m(%f,%f) = \n",i,j);
+	gsl_matrix* matrix_hermit_im = gsl_matrix_alloc(5,5);
+	gsl_matrix_transpose_memcpy(matrix_hermit_im, matrix_sample_im); //Transpose imaginary part
+	gsl_matrix_scale(matrix_hermit_im, -1); //Flip imaginary part
 
-	gsl_matrix_complex_free(matrix);
-	gsl_matrix_complex_free(matrix_result);
-	gsl_matrix_complex_free(matrix_hermit);
+	// d: type double, ge: general , mm: matrix multiplication. Result stored in 'C' (last argument)
+	gsl_matrix* result_re = gsl_matrix_alloc(5,5); 
+	gsl_blas_dgemm(CblasNoTrans, CblasNoTrans, 1, matrix_sample_re,matrix_hermit_re,0,result_re);
+
+	//                                        subtract                             from previous
+	gsl_blas_dgemm(CblasNoTrans, CblasNoTrans, -1, matrix_sample_im,matrix_hermit_im,1,result_re);
+	
+
+
+	gsl_matrix* result_im = gsl_matrix_alloc(5,5);
+	gsl_blas_dgemm(CblasNoTrans, CblasNoTrans, 1, matrix_sample_re,matrix_hermit_im,0,result_im);
+	
+	//                                        add                             to previous
+	gsl_blas_dgemm(CblasNoTrans, CblasNoTrans, 1, matrix_sample_im,matrix_hermit_re,1,result_im);
 		
+	//Divide results by n: number of samples
+	double scale = 1./n;
+	gsl_matrix_scale(result_im, scale);
+	gsl_matrix_scale(result_re, scale);
+
+
+
 	
-	return 0;
+	return 1;
 
 }
 
