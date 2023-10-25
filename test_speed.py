7af12fb2c9bb7c734a09044c1b0253a39368da7b
Joel Boyer <joel.boyer@mail.mcgill.ca>
2023-10-25 17:33:55 -0400

    test_speed.py tested and works

--- test_speed.py
+++ test_speed.py
@@ -5,19 +5,22 @@ import numpy as np
 # Define the path to the C binary you want to execute
 binary_paths = ["./sample_cov_base","./sample_cov_ftree","./sample_cov_autovectorize_ftree","./sample_cov_autovectorize_no_ftree"]  # Replace with the actual path to your C binary
 
-test_range= 10
+test_range= 1000
 
 average_times=[]
 
+i = 0
 try:
     for bf in binary_paths:
+        print("Computing " + "binary_paths[i]" + f" for {test_range} samples" + f"({i+1} out of {len(binary_paths)})")
         arr=np.zeros(test_range)
-        for i in range(test_range)
+        for i in range(test_range):
             return_code = subprocess.call(bf)
             arr[i]=return_code
-
-        average_times.append( np.mean(arr)  )
-
+        
+        av=np.mean(arr)
+        average_times.append( av )
+        i+=1
 
 
 except subprocess.CalledProcessError as e:
